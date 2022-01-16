1ddf3983f2b8c784839b3a6af60f7e339a7e2b9d
aq2630 <a.qadirsscl@gmail.com>
2022-01-17 01:30:27 +0500

    bg for roadmap

--- components/NewSection5/index.js
+++ components/NewSection5/index.js
@@ -112,7 +112,7 @@ const Wrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url("/images/roadmap-bg-2.jpeg");
+  background-image: url("/images/roadmap-bg-3.png");
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
